6c26c2b6c132e298365fb5c9600cd56b8d01ef14
Yukinoshita <53087601+FujiwaraChika0303@users.noreply.github.com>
2026-02-28 18:00:39 +1100

    1

--- components/MonitorList.tsx
+++ components/MonitorList.tsx
@@ -50,7 +50,7 @@ export default function MonitorList({
   }
   const itemVariants = {
     hidden: { opacity: 0, y: 14 },
-    show: { opacity: 1, y: 0, transition: { duration: 0.35, ease: 'easeOut' } },
+    show: { opacity: 1, y: 0, transition: { duration: 0.35 } },
   }
   const group = pageConfig.group
   const groupedMonitor = group && Object.keys(group).length > 0
